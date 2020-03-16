9aa234c79937d99e3180fe4536fa73643f40c3f7
luka <kaj.lund@gmail.com>
2020-03-16 21:33:35 +0200

    Lecture 10

--- 02/sandbox.js
+++ 02/sandbox.js
@@ -15,12 +15,11 @@ var score = 75;
 console.log(score); // => 75
 
 // Strings
-
 let message = "Hello World!";
 console.log(message); // => Hello World!
 console.log(message[0]); // => H
 
-// String concatenation
+// String Concatenation
 let firstName = 'Kaj';
 let lastName = 'Lund';
 let fullName = firstName + ' ' + lastName;
@@ -36,3 +35,9 @@ console.log(result); // => 'kaj lund'
 
 let idx = fullName.indexOf('L');
 console.log(idx); // => 4
+
+let email = 'luka@networknirvana.co.uk';
+console.log(email.lastIndexOf('n'));  // => 17
+console.log(email.slice(0, 4));       // start, stop => luka
+console.log(email.substr(0, 4));      // start, count => luka
+console.log(email.replace('n', 'x')); // replace first n with x unless regex => luka@xetworknirvana.co.uk
